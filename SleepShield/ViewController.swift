d12988a4960a520175fdb8266e9c63af0aef8fba
Matthew Krager <matthewkrager@gmail.com>
2020-05-02 10:19:03 -0700

    Initial Commit

---
+++ SleepShield/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SleepShield
+//
+//  Created by Matthew Krager on 5/2/20.
+//  Copyright © 2020 Matthew Krager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
